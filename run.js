83024a9bdf71397350980c4edd79a6b7e70d3851
it-22-tip <it.22.tip@gmail.com>
2018-10-31 10:53:42 +0700

    working

--- run.js
+++ run.js
@@ -1,8 +1,3 @@
-/* import { spawn } from 'child_process';
-var pro = spawn('protractor', paramsForProtractorSpawn);
-  pro.stdout.on('data', (data) => {
-    console.log('stdout: ' + data);
-  }); */
 import path from 'path';
 var conf = path.resolve(__dirname, 'conf.js');
 const Launcher = require('protractor/built/launcher');
